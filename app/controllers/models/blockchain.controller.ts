389a254130f60a1157a820b7ebfabb596c957d63
ThPadelis <padelis.theodosiou@gmail.com>
2020-11-30 15:01:12 +0200

    Minor changes
    - Created controller for the blockchain methods
    - Deleted unwanted file
    - Updated the blockchain.routes.ts file

---
+++ app/controllers/models/blockchain.controller.ts
@@ -0,0 +1,65 @@
+import { NextFunction, Request, Response } from "express";
+import {
+  getAccounts,
+  getBalance,
+  getReceipt,
+  transfer,
+} from "../../connection/blockchain";
+import logger from "../../utils/logger";
+
+export class BlockchainController {
+  async getAccounts(request: Request, response: Response, next: NextFunction) {
+    try {
+      const accounts = await getAccounts();
+      response.status(200).json(accounts);
+    } catch (error) {
+      logger.error(error.message);
+      console.log(error.error);
+      next(error);
+    }
+  }
+
+  async getAccountBalance(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { account = "" } = request.params;
+      const balance = await getBalance(account);
+      response.status(200).json(balance);
+    } catch (error) {
+      logger.error(error.message);
+      next(error.message);
+    }
+  }
+
+  async transfer(request: Request, response: Response, next: NextFunction) {
+    try {
+      const { sender = "", recipient = "", amount = "" } = request.body;
+      const receipt = await transfer({
+        sender,
+        receiver: recipient,
+        amount,
+      });
+      response.status(200).json(receipt);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getTransactionReceipt(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { hash = "" } = request.params;
+      const receipt = await getReceipt(hash);
+      response.status(200).json(receipt);
+    } catch (error) {
+      logger.error(error.message);
+      next(error.message);
+    }
+  }
+}
--- app/routes/blockchain.routes.ts
+++ app/routes/blockchain.routes.ts
@@ -1,80 +1,27 @@
-import { NextFunction, Request, Response, Router } from "express";
-import {
-  getAccounts,
-  getBalance,
-  getReceipt,
-  transfer,
-} from "../connection/blockchain";
-import logger from "../utils/logger";
+import { Router } from "express";
+import { BlockchainController } from "../controllers/models/blockchain.controller";
 
 const router: Router = Router();
 export class BlockchainRoutes {
-  constructor() {}
+  _blockchainController: BlockchainController;
+  constructor() {
+    this._blockchainController = new BlockchainController();
+  }
 
   get routes() {
+    const controller = this._blockchainController;
+
     // Fetches all available accounts
-    router.get(
-      "/accounts",
-      async (request: Request, response: Response, next: NextFunction) => {
-        try {
-          const accounts = await getAccounts();
-          response.status(200).json(accounts);
-        } catch (error) {
-          logger.error(error.message);
-          console.log(error.error);
-          next(error);
-        }
-      }
-    );
+    router.get("/accounts", controller.getAccounts);
 
     // Fetches the balance of the given account address
-    router.get(
-      "/balance/:account",
-      async (request: Request, response: Response, next: NextFunction) => {
-        try {
-          const { account = "" } = request.params;
-          const balance = await getBalance(account);
-          response.status(200).json(balance);
-        } catch (error) {
-          logger.error(error.message);
-          next(error.message);
-        }
-      }
-    );
+    router.get("/balance/:account", controller.getAccountBalance);
 
-    // Transfer coins from one account to other
-    router.post(
-      "/transfer",
-      async (request: Request, response: Response, next: NextFunction) => {
-        try {
-          const { sender = "", recipient = "", amount = "" } = request.body;
-          const receipt = await transfer({
-            sender,
-            receiver: recipient,
-            amount,
-          });
-          response.status(200).json(receipt);
-        } catch (error) {
-          console.log("ERROR", error);
+    // Transfers coins from one account to other
+    router.post("/transfer", controller.transfer);
 
-          next(error);
-        }
-      }
-    );
-
-    router.get(
-      "/receipt/:hash",
-      async (request: Request, response: Response, next: NextFunction) => {
-        try {
-          const { hash = "" } = request.params;
-          const receipt = await getReceipt(hash);
-          response.status(200).json(receipt);
-        } catch (error) {
-          logger.error(error.message);
-          next(error.message);
-        }
-      }
-    );
+    // Fetches details about a transaction
+    router.get("/receipt/:hash", controller.getTransactionReceipt);
 
     return router;
   }
